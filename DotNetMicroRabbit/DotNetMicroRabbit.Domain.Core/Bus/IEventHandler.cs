9375347e2ca55fe3fe67e43450a1bede33305179
Lucas Rombaldi <lucas.rombaldi@focco.com.br>
2020-02-20 18:42:06 -0300

    Initial commit for basic core structure.

---
+++ DotNetMicroRabbit/DotNetMicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using DotNetMicroRabbit.Domain.Core.Events;
+using System.Threading.Tasks;
+
+namespace DotNetMicroRabbit.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
