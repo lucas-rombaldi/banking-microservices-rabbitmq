8c4a47fc3c4996e6799ea6ffa2bc5d480e3839b2
Lucas Rombaldi <lucas.rombaldi@focco.com.br>
2020-02-20 19:20:12 -0300

    Initial IoC Container Project

---
+++ DotNetMicroRabbit/DotNetMicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using DotNetMicroRabbit.Domain.Core.Bus;
+using DotNetMicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetMicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
